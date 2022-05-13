1a63473f0651f2943f49bb8db62762a7be051896
Cristobal Pérez García <CRISTOBAL.PEREZ@ucr.ac.cr>
2022-05-13 08:10:16 -0600

    creacion de algunos documentos

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function css(){
+function css(done){
 
 src('src/scss/app.scss')
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(dest('build/css'))
+done();
+}
 
+function dev(){
+watch('src/scss/app.scss', css)
 }
 
 exports.css = css;
+exports.dev = dev;
